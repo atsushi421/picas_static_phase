4314945380e06c67155cdb09be30661c128db9d6
atsushi421 <atsushi.yano.2@tier4.jp>
2023-10-15 11:20:02 +0900

    feat: create dags from yaml
    Signed-off-by: atsushi421 <atsushi.yano.2@tier4.jp>

--- picas_static_phase/src/dag_parser.rs
+++ picas_static_phase/src/dag_parser.rs
@@ -2,7 +2,114 @@ use crate::callback::{RegularCallback, TimerCallback};
 use crate::callback_group::CallbackGroup;
 use crate::chain::Chain;
 use std::cell::RefCell;
+use std::fs;
 use std::rc::Rc;
+
+use crate::graph_extension::{GraphExtension, NodeData};
+
+use petgraph::{graph::Graph, prelude::*};
+use std::path::PathBuf;
+use yaml_rust::YamlLoader;
+
+fn load_yaml(file_path: &str) -> Vec<yaml_rust::Yaml> {
+    if !file_path.ends_with(".yaml") && !file_path.ends_with(".yml") {
+        panic!("Invalid file type: {}", file_path);
+    }
+    let file_content = fs::read_to_string(file_path).unwrap();
+    YamlLoader::load_from_str(&file_content).unwrap()
+}
+
+fn create_dag_from_yaml(file_path: &str) -> Graph<NodeData, i32> {
+    let yaml_doc = load_yaml(file_path)[0];
+    let mut dag = Graph::<NodeData, i32>::new();
+
+    // add nodes to dag
+    for node in yaml_doc["nodes"].as_vec().unwrap() {
+        let mut id = "0".to_string();
+        let mut callback_group_id = "0".to_string();
+        let mut wcet = 0;
+        let mut period = None;
+        for (key, value) in node.as_hash().unwrap() {
+            let key_str = key.as_str().unwrap();
+            if key_str == "id" {
+                id = value.as_str().unwrap().to_string();
+            } else if key_str == "callback_group_id" {
+                callback_group_id = value.as_str().unwrap().to_string();
+            } else if key_str == "wcet" {
+                wcet = value.as_i64().unwrap() as i32;
+            } else if key_str == "period" {
+                period = Some(value.as_i64().unwrap() as i32);
+            } else {
+                panic!("Invalid key: {}", key_str);
+            }
+        }
+        dag.add_node(NodeData::new(id, callback_group_id, wcet, period));
+    }
+
+    // add edges to dag
+    for link in yaml_doc["links"].as_vec().unwrap() {
+        let source = link["source"].as_i64().unwrap() as usize;
+        let target = link["target"].as_i64().unwrap() as usize;
+        let dummy_communication_time = 0;
+
+        dag.add_edge(
+            NodeIndex::new(source),
+            NodeIndex::new(target),
+            dummy_communication_time,
+        );
+    }
+    dag
+}
+
+fn get_yaml_paths_from_dir(dir_path: &str) -> Vec<String> {
+    if !std::fs::metadata(dir_path).unwrap().is_dir() {
+        panic!("Not a directory");
+    }
+    let mut file_path_list = Vec::new();
+    for dir_entry_result in PathBuf::from(dir_path).read_dir().unwrap() {
+        let path = dir_entry_result.unwrap().path();
+        let extension = path.extension().unwrap();
+        if extension == "yaml" || extension == "yml" {
+            file_path_list.push(path.to_str().unwrap().to_string());
+        }
+    }
+    if file_path_list.is_empty() {
+        panic!("No YAML file found in {}", dir_path);
+    }
+    file_path_list
+}
+
+/// load yaml files and return a DAGSet (dag list)
+///
+/// # Arguments
+///
+/// *  `dir_path` - dir path for yaml files
+///
+/// # Returns
+///
+/// *  `dag_set` - dag list (petgraph vector)
+///
+/// # Example
+///
+/// ```
+/// use lib::dag_creator::create_dag_set_from_dir;
+/// let dag_set = create_dag_set_from_dir("tests/sample_dags/multiple_yaml");
+/// let first_node_num = dag_set[0].node_count();
+/// let first_edge_num = dag_set[0].edge_count();
+/// let first_node_exe_time = dag_set[0][dag_set[0].node_indices().next().unwrap()].params["execution_time"];
+/// ```
+pub fn create_dag_set_from_dir(dir_path: &str) -> Vec<Graph<NodeData, i32>> {
+    let mut file_path_list = get_yaml_paths_from_dir(dir_path);
+    file_path_list.sort();
+
+    let mut dag_set: Vec<Graph<NodeData, i32>> = Vec::new();
+    for (dag_id, file_path) in file_path_list.iter().enumerate() {
+        let mut dag = create_dag_from_yaml(file_path);
+        dag.set_dag_param("dag_id", dag_id as i32);
+        dag_set.push(dag);
+    }
+    dag_set
+}
 
 pub fn parse_dag() -> (Vec<RefCell<CallbackGroup>>, Vec<Chain>) {
     // [chain0] timer0 -> regular0 -> regular1
